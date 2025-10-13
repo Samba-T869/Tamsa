00cf1eb4759a7bd751f328056552bc0af7cc8cfd
Samba-T869 <Kimwagangalula45@gmail.com>
2025-10-13 18:57:53 +0300

    Update tamsa.py

--- tamsa.py
+++ tamsa.py
@@ -480,4 +480,4 @@ def add_leader_form():
     return render_template('add_leader.html')
     
 if __name__ == '__main__':
-    app.run(port='8000', debug=True)
+    app.run(host='0.0.0.0', port='8000', debug=False)
